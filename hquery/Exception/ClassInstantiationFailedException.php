ffdd6b2b99a42c56ee9703a66940033c3cdc6d5d
amin_tado <amintado@gmail.com>
2020-06-06 19:41:20 +0430

    Start Push Repository To Git

---
+++ hquery/Exception/ClassInstantiationFailedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace system\lib\hquery\Exception;
+
+use system\lib\hquery\Exception;
+
+/**
+ * Thrown when a class fails to instantiate.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+final class ClassInstantiationFailedException extends \RuntimeException implements Exception
+{
+}
---
+++ hquery/NotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace system\lib\hquery;
+
+/**
+ * Thrown when a discovery does not find any matches.
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ *
+ * @deprecated since since version 1.0, and will be removed in 2.0. Use {@link \system\lib\hquery\Exception\NotFoundException} instead.
+ */
+final class NotFoundException extends \system\lib\hquery\Exception\NotFoundException
+{
+}
